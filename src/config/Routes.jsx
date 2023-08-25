056dab256c17171e3a4a22e0cc427468a87557e9
Jun Ukemori <jukemori@gmail.com>
2023-08-26 07:51:15 +0900

    set routes

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,7 +1,18 @@
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 
+import Home from "../pages/Home";
+import Catalog from "../pages/Catalog";
+import Detail from "../pages/Detail";
+
 const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <RouterRoutes>
+      <Route path="/:category/search/:keyword" element={<Catalog />} />;
+      <Route path="/:category/:id" element={<Detail />} />;
+      <Route path="/:category" element={<Catalog />} />;
+      <Route path="/" exact element={<Home />} />;
+    </RouterRoutes>
+  );
 };
 
 export default Routes;
