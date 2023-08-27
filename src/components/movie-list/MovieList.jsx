3d9357aa03ef7cdfa3e452cd06f7aa5af8104b2c
Jun Ukemori <jukemori@gmail.com>
2023-08-28 08:58:02 +0900

    add api fetching

---
+++ src/components/movie-list/MovieList.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+import "./movie-list.scss";
+
+import { SwiperSide, Swiper } from "swiper/react";
+import { Link } from "react-router-dom";
+
+import Button from "../button/Button";
+
+import tmdbApi, { category } from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
+
+const MovieList = (props) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      const params = {};
+
+      if (props.type !== "similar") {
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(props.type, { params });
+            break;
+          default:
+            response = await tmdbApi.getTvList(props.type, { params });
+        }
+      } else {
+        response = await tmdbApi.similar(props.category, props.id);
+      }
+      setItems(response.results);
+    };
+    getList();
+  }, []);
+
+  return <div>MovieList</div>;
+};
+
+MovieList.propTypes = {
+  id: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
+export default MovieList;
