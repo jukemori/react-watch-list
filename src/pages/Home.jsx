0ecd58b4e6935d510fbe76e601ce19277882455a
Jun Ukemori <jukemori@gmail.com>
2023-08-27 14:42:28 +0900

    set home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/button/Button";
+
 import HeroSlide from "../components/hero-slide/HeroSlide";
 
 const Home = () => {
   return (
     <>
       <HeroSlide />
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View More</OutlineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
