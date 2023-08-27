a343281bec68498f7be9e7687e1ebcfd56d802c2
Jun Ukemori <jukemori@gmail.com>
2023-08-27 13:07:12 +0900

    add swiper

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from "react";
+
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import tmdbApi, { category, movieType } from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
+
+import "./hero-slide.scss";
+
 const HeroSlide = () => {
-  return <div className="hero-slide">HeroSlide</div>;
+  const [movieItems, setMovieItems] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const params = { page: 1 };
+      try {
+        const response = await tmdbApi.getMoviesList(movieType.popular, {
+          params,
+        });
+        setMovieItems(response.results.slice(0, 4));
+        console.log(response);
+      } catch {
+        console.log("error");
+      }
+    };
+    getMovies();
+  }, []);
+
+  return (
+    <div className="hero-slide">
+      <Swiper
+        modules={[Autoplay]}
+        grabCursor={true}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 3000 }}
+      >
+        {movieItems.map((item, i) => (
+          <SwiperSlide key={i}>
+            {({ isActive }) => (
+              <img src={apiConfig.originalImage(item.backdrop_path)} />
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default HeroSlide;
