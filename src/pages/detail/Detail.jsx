ae5106fbddf96c45d9a66c90c88f4e9248a0ffc5
Jun Ukemori <jukemori@gmail.com>
2023-08-29 10:55:48 +0900

    add video

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -6,6 +6,9 @@ import apiConfig from "../../api/apiConfig";
 
 import "./detail.scss";
 import CastList from "./CastList";
+import VideoList from "./VideoList";
+
+import MovieList from "../../components/movie-list/MovieList";
 
 const Detail = () => {
   const { category, id } = useParams();
@@ -61,6 +64,17 @@ const Detail = () => {
                 </div>
                 <CastList id={item.id} category={category} />
               </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="section mb-3">
+              <VideoList id={item.id} />
+            </div>
+            <div className="section mb-3">
+              <div className="section__header mb-2">
+                <h2>Similar</h2>
+              </div>
+              <MovieList category={category} type="similar" id={item.id} />
             </div>
           </div>
         </>
